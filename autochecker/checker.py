34288a03da60d0f06c48b0b8b8b98da153e61c9a
dldhk97 <dldhk97@gmail.com>
2021-09-12 15:22:02 +0900

    Add random sleep method

--- autochecker/checker.py
+++ autochecker/checker.py
@@ -1,10 +1,11 @@
-import os, sys
+import os, sys, time, random
 from .driver.driver_utils import load_driver
 from .page.login_page import login
 from .page.check_page import check_all
 
 def parse():
     try:
+        _random_sleep()
         _check_env()
         url = os.environ.get('SELF_CHECK_URL')
 
@@ -31,3 +32,9 @@ def _check_env():
         raise Exception('No .env file! Please create .env file using .env_example')
     if len(sys.argv) < 3:
         raise Exception('Invalid args! Please run with args (USER_ID, USER_PASSWORD)')
+
+def _random_sleep():
+    sleep_time = random.randrange(0, 5)
+    print('Now sleep for ' + str(sleep_time) + 's')
+    time.sleep(sleep_time)
+    print('Now awaked')
